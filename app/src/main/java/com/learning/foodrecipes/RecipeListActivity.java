6ecb81b189eb136ecf2b79c775e735bfce7d81ff
Saravanan <saravinfotech@gmail.com>
2019-07-27 10:38:57 +0100

    Test Retrofit APIs

--- app/src/main/java/com/learning/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/learning/foodrecipes/RecipeListActivity.java
@@ -1,10 +1,26 @@
 package com.learning.foodrecipes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.learning.foodrecipes.models.Recipe;
+import com.learning.foodrecipes.requests.RecipeAPI;
+import com.learning.foodrecipes.requests.ServiceGenerator;
+import com.learning.foodrecipes.requests.response.RecipeSearchResponse;
+import com.learning.foodrecipes.util.Constants;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.internal.EverythingIsNonNull;
+
 public class RecipeListActivity extends BaseActivity {
+
+    private String TAG = "RecipeListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,12 +29,57 @@ public class RecipeListActivity extends BaseActivity {
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mProgressBar.getVisibility() == View.VISIBLE){
+                /*if(mProgressBar.getVisibility() == View.VISIBLE){
                     showProgressBar(false);
                 }else{
                     showProgressBar(true);
-                }
+                }*/
+
+                testResponse();
             }
         });
+    }
+
+
+    public void testResponse() {
+        RecipeAPI recipeAPI = ServiceGenerator.getRecipeAPI();
+
+        Call<RecipeSearchResponse> responseCall = recipeAPI
+                .searchRecipe(
+                        Constants.API_KEY,
+                        "cheese",
+                        "1"
+                );
+
+        responseCall.enqueue(new Callback<RecipeSearchResponse>() {
+            @Override
+            @EverythingIsNonNull
+            public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+                Log.d(TAG, "onResponse : Server Response" + response.toString());
+                if (response.code() == 200) {
+                    if (null != response.body() && null != response.body().getRecipes()) {
+                        List<Recipe> recipeList = response.body().getRecipes();
+                        for (Recipe recipe : recipeList) {
+                            Log.d(TAG, recipe.getTitle());
+                        }
+                    }
+                } else {
+                    try {
+                        if (null != response.errorBody()) {
+                            Log.d(TAG, "onResponse :" + response.errorBody().string());
+                        }
+                    } catch (IOException e) {
+                        Log.d(TAG, e.getLocalizedMessage());
+                    }
+                }
+            }
+
+            @Override
+            @EverythingIsNonNull
+            public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+                Log.e(TAG, "API Failed");
+            }
+        });
+
     }
 }
